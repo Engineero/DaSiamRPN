1adeccfc49deeeeee3689b082b3c9d38dd452013
Nathan Toner <nathan.toner@gmail.com>
2023-02-24 14:46:05 -0700

    Added examples for WFOV data.

--- code/demo.py
+++ code/demo.py
@@ -9,19 +9,28 @@ import glob, cv2, torch
 import numpy as np
 from os.path import realpath, dirname, join
 
-from net import SiamRPNvot
+from net import SiamRPNBIG
 from run_SiamRPN import SiamRPN_init, SiamRPN_track
 from utils import get_axis_aligned_bbox, cxy_wh_2_rect
 
 # load net
-net = SiamRPNvot()
-net.load_state_dict(torch.load(join(realpath(dirname(__file__)), 'SiamRPNVOT.model')))
-net.eval().cuda()
+net = SiamRPNBIG()
+if torch.cuda.is_available():
+    net.load_state_dict(torch.load(join(realpath(dirname(__file__)), 'SiamRPNBIG.model')))
+    net.eval().cuda()
+else:
+    net.load_state_dict(torch.load(join(realpath(dirname(__file__)), 'SiamRPNBIG.model'),
+                                   map_location=torch.device('cpu')))
+    net.eval().cpu()
 
 # image and init box
-image_files = sorted(glob.glob('./bag/*.jpg'))
-init_rbox = [334.02,128.36,438.19,188.78,396.39,260.83,292.23,200.41]
-[cx, cy, w, h] = get_axis_aligned_bbox(init_rbox)
+# image_files = sorted(glob.glob('./bag/*.jpg'))
+# init_rbox = [334.02,128.36,438.19,188.78,396.39,260.83,292.23,200.41]
+# [cx, cy, w, h] = get_axis_aligned_bbox(init_rbox)
+# image_files = sorted(glob.glob('/Users/nathantoner/Cloudstone/Data/WFOV-Daytime/object-a-images/*.jpg'))
+# [cx, cy, w, h] = [425, 40, 25, 25]
+image_files = sorted(glob.glob('/Users/nathantoner/Cloudstone/Data/WFOV-Daytime/airplane1-images/*.jpg'))
+[cx, cy, w, h] = [1880, 870, 40, 40]
 
 # tracker init
 target_pos, target_sz = np.array([cx, cy]), np.array([w, h])
@@ -39,6 +48,6 @@ for f, image_file in enumerate(image_files):
     res = [int(l) for l in res]
     cv2.rectangle(im, (res[0], res[1]), (res[0] + res[2], res[1] + res[3]), (0, 255, 255), 3)
     cv2.imshow('SiamRPN', im)
-    cv2.waitKey(1)
+    cv2.waitKey(5)
 
 print('Tracking Speed {:.1f}fps'.format((len(image_files)-1)/(toc/cv2.getTickFrequency())))
